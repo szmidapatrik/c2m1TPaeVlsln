db8a37776744b2c72ea4ef54cc05533ccd41a6b3
szmidapatrik <patriksz25@gmail.com>
2024-09-18 21:08:18 +0200

    CS2.preprocess NormalizerDictionary extra functions added

--- package/CS2/preprocess/normalizer_dictionary.py
+++ package/CS2/preprocess/normalizer_dictionary.py
@@ -27,8 +27,31 @@ class Dictionary:
     # REGION: Public methods - Build dictionary
     # --------------------------------------------------------------------------------------------
 
-    # Build scaling dictionary
+    # Build scaling dictionary by file lsit
     def build_dictionary(
+        self, 
+        folder_path,
+        file_list,
+
+    ):
+        """
+        Builds a dictionary of min and max values for each column in the dataset by reading the dictionary files given in the file_list parameter.
+        
+        Parameters:
+            - folder_path: str: The path to the folder containing the dictionary files.
+            - file_list: list: A list of dictionary file names to be read and used in the dictionary building process.
+        """
+
+        # 1. Build the player-variant scaling dictionary
+        scaling_dict = self.__scaling_dict_player_variant__(folder_path, file_list)
+
+        # 2. Make it player-invariant
+        scaling_dict = self.__scaling_dict_player_invariant__(scaling_dict)
+
+        return scaling_dict
+
+    # Build scaling dictionary by folder path
+    def build_dictionary_from_path(
         self, 
         folder_path,
         convention_type='prefix',
@@ -50,7 +73,7 @@ class Dictionary:
             return None
 
         # 1. Build the player-variant scaling dictionary
-        scaling_dict = self.__scaling_dict_1__(folder_path, convention_type, convention_value)
+        scaling_dict = self.__scaling_dict_player_variant_by_path__(folder_path, convention_type, convention_value)
 
         # 2. Make it player-invariant
         scaling_dict = self.__scaling_dict_player_invariant__(scaling_dict)
@@ -73,12 +96,90 @@ class Dictionary:
 
 
 
+    # Merge dictionaries
+    def merge_dictionaries(
+        self,
+        dictionary_list: list,
+    ):
+        """
+        Merges a list of dictionaries into a single dictionary.
+        
+        Parameters:
+            - dictionary_list: list: A list of dictionaries to be merged.
+        """
+
+        # Initialize the scaling dictionary
+        scaling_dict = dictionary_list[0]
+
+        # Iterate through the dictionary_list and append the data to the scaling dictionary
+        for dictionary in dictionary_list[1:]:
+
+            scaling_dict['other_min'] = dictionary['min']
+            scaling_dict['other_max'] = dictionary['max']
+
+            # Update 'min' and 'max' columns
+            scaling_dict['min'] = scaling_dict[['min', 'other_min']].min(axis=1)
+            scaling_dict['max'] = scaling_dict[['max', 'other_max']].max(axis=1)
+
+            # Free up memory
+            del dictionary
+
+            # Drop the 'other_min' and 'other_max' columns
+            scaling_dict.drop(columns=['other_min', 'other_max'], inplace=True)
+
+        return scaling_dict
+
+
     # --------------------------------------------------------------------------------------------
     # REGION: Private methods
     # --------------------------------------------------------------------------------------------
 
-    # 1. Create the player-variant scaling dictionary
-    def __scaling_dict_player_variant__(self, folder_path, convention_type, convention_value):
+    # 0. Read dictionary and fix values
+    def __read_dictionary__(self, file_path):
+            
+        # Read the dictionary file
+        dictionary = pd.read_csv(file_path)
+
+        # Impute inf values
+        dictionary.loc[dictionary['column'].str.endswith('_ammo_left_%'), 'min'] = 0
+        dictionary.loc[dictionary['column'].str.endswith('_ammo_left_%'), 'max'] = 1
+
+        # Convert the 'min' and 'max' columns to numeric
+        dictionary['min'] = dictionary['min'].replace({'False': 0, 'True': 1}).astype(float)
+        dictionary['max'] = dictionary['max'].replace({'False': 0, 'True': 1}).astype(float)
+
+        return dictionary
+
+
+
+    # 1. Create the player-variant scaling dictionary 
+    def __scaling_dict_player_variant__(self, folder_path, file_list):
+
+        # Initialize the scaling dictionary
+        scaling_dict = self.__read_dictionary__(folder_path + file_list[0])
+
+        # Iterate through the file_list and append the data to the scaling dictionary
+        for file in file_list[1:]:
+
+            temp_dict = self.__read_dictionary__(folder_path + file)
+
+            scaling_dict['other_min'] = temp_dict['min']
+            scaling_dict['other_max'] = temp_dict['max']
+
+            # Update 'min' and 'max' columns
+            scaling_dict['min'] = scaling_dict[['min', 'other_min']].min(axis=1)
+            scaling_dict['max'] = scaling_dict[['max', 'other_max']].max(axis=1)
+
+            # Free up memory
+            del temp_dict
+
+            # Drop the 'other_min' and 'other_max' columns
+            scaling_dict.drop(columns=['other_min', 'other_max'], inplace=True)
+
+        return scaling_dict
+
+    # 1. Create the player-variant scaling dictionary by folder path
+    def __scaling_dict_player_variant_by_path__(self, folder_path, convention_type, convention_value):
         
         # Read all files in the folder_path and filter the ones with the given postfix
         files = os.listdir(folder_path)
@@ -109,6 +210,8 @@ class Dictionary:
             scaling_dict.drop(columns=['other_min', 'other_max'], inplace=True)
 
         return scaling_dict
+
+
 
     # 2. Create the player-invariant scaling dictionary
     def __scaling_dict_player_invariant__(self, scaling_dict: pd.DataFrame):
